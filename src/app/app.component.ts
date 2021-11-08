9416e29f6c731eb5221cd08994edd0030364186e
AndrewJBateman <gomezbateman@yahoo.com>
2021-11-08 18:52:09 +0100

    feat: PWA software updates prompt added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  public installPrompt = null;
+  constructor(private swUpdate: SwUpdate) {
+    if (this.swUpdate.available) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('A new sw version is available, load it?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
+
+  getInstallPrompt() {
+    window.addEventListener('beforeinstallPrompt', (e) => {
+      e.preventDefault();
+      this.installPrompt = e;
+    });
+  }
+
+  askUserToInstallApp() {
+    this.installPrompt.prompt();
+  }
 }
